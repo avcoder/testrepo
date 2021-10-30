89145763593528ab3efb1d2f3aca3bed57b39bbc
avcoder <code.rom7.6@gmail.com>
2021-10-30 09:14:27 -0400

    Edits

--- functions/coords.js
+++ functions/coords.js
@@ -1,8 +1,8 @@
 let crdArray = [0, 0];
 
 exports.handler = async (event) => {
-  const x = event.queryStringParameters.lat;
-  const y = event.queryStringParameters.lng;
+  const x = event.queryStringParameters.lat || crdArray[0];
+  const y = event.queryStringParameters.lng || crdArray[1];
 
   console.log(`x is ${x}; y is ${y}`);
 
